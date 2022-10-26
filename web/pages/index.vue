047100cfaa6f2dc9994c0350b76f0b5c31a144e2
Ryota NIINOMI Takemoto <r21nomi@gmail.com>
2022-10-26 10:10:32 +0900

    Hide cursor when no operation

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="topPage" :class="{ sp: $device.isMobile }">
+    <div is="style" v-if="shouldHideCursor">* { cursor: none !important; }</div>
     <ArtCanvas />
   </div>
 </template>
@@ -11,7 +12,31 @@ import ArtCanvas from '~/components/ArtCanvas.vue'
 @Component({
   components: { ArtCanvas },
 })
-export default class TopPage extends Vue {}
+export default class TopPage extends Vue {
+  private shouldHideCursor: boolean = true
+  private cursorTimer: any | null = null
+
+  mounted() {
+    window.addEventListener('mousemove', this.onMousemove)
+  }
+
+  beforeDestroy() {
+    if (this.cursorTimer) {
+      clearTimeout(this.cursorTimer)
+    }
+  }
+
+  private onMousemove() {
+    this.shouldHideCursor = false
+
+    if (this.cursorTimer) {
+      clearTimeout(this.cursorTimer)
+    }
+    this.cursorTimer = setTimeout(() => {
+      this.shouldHideCursor = true
+    }, 1000)
+  }
+}
 </script>
 
 <style scoped lang="stylus">
